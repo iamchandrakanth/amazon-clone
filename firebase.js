ae32f7a7b60cf16a02037ace6fa4f26ba9f07142
iamchandrakanth <ccmchandrakanth5@gmail.com>
2021-05-23 15:36:57 +0530

    Stripe & Order page

--- firebase.js
+++ firebase.js
@@ -8,3 +8,11 @@ const firebaseConfig = {
     messagingSenderId: "1030826785310",
     appId: "1:1030826785310:web:15e60e209baa71c8d1cc0f"
   };
+
+  const app = !firebase.apps.length 
+  ? firebase.initializeApp(firebaseConfig)
+  : firebase.app();
+
+  const db = app.firestore();
+
+  export default db;
